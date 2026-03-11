ea9497d91cdc3a45a8d3c8bd64c431c1e275bfbb
Brandon Daniel Robles Jimenez <brandon-95cr@hotmail.com>
2026-03-11 16:36:19 -0600

    Modelos Libro y Controlador con lista estática.

---
+++ Controllers/LibroController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BibliotecaMVC.Models;
+
+namespace BibliotecaMVC.Controllers
+{
+    public class LibroController
+    {
+        // Lista estática para guardar los libros
+        public static List<Libro> libros = new List<Libro>();
+    }
+}
---
+++ Models/Libro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; // Importa las anotaciones para validación de datos
+
+
+namespace BibliotecaMVC.Models
+{
+    public class Libro
+    { // El campo Codigo es obligatorio
+        [Required(ErrorMessage = "Código requerido")]
+        public string Codigo { get; set; }
+
+        // El campo Titulo es obligatorio y máximo 100 caracteres
+        [Required, StringLength(100)]
+        public string Titulo { get; set; }
+
+        // Campo opcional para el nombre del autor
+        public string Autor { get; set; }
+    }
+}
